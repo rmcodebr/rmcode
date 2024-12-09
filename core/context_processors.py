06994f718c64f0aceaba09f7cf55d0f9f329656b
rmcodebr <rmcodebr@gmail.com>
2024-12-09 19:43:32 -0300

    is_development fixed

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,6 @@
 def is_development(request):
-    allowed_hosts = ["localhost", "rm", "127.0.0.1"]
+    print(request.get_host())
+    allowed_hosts = ["localhost:8000", "127.0.0.1:8000"]
     return {
         'is_development': request.get_host() in allowed_hosts
     }
